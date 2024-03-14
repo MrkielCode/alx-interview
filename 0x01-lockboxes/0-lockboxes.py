f22aa1641242caad9624803384c3137f50c1e7d6
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-03-14 17:59:34 +0100

    lootboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 '''LockBoxes Challenge'''
 
@@ -31,4 +30,3 @@ def canUnlockAll(boxes):
         if i not in opened_boxes and i != 0:
             return False
     return True
-
